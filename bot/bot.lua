8b13a23c775c02f0111803cdb378de12f803856b
telespead <bot.tele.spead@gmail.com>
2016-03-16 16:34:37 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -245,7 +245,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {our_id},
+    sudo_users = {96700621},
     admin_users = {},
     disabled_channels = {}
   }
